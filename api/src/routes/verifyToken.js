64dc2a9c23470d336e21df726cbb9d82613e8661
aleksanderwawrzyniak <aleksanderwawrzyniak98@gmail.com>
2020-04-01 23:23:05 +0200

    simple work and fixes with authentication done, remove ejs from package.json

--- api/src/routes/verifyToken.js
+++ api/src/routes/verifyToken.js
@@ -1,14 +1,16 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-  const token = req.header('auth-token');
+  const token = req.header('x-auth-token');
   if (!token) return res.status(401).send('Access denied');
 
   try {
-    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET, {
+      algorithms: ['HS256'],
+    });
     req.user = verified;
     next();
   } catch (err) {
-    res.status(400).send('Invalid token');
+    res.status(401).send('Invalid token');
   }
 };
