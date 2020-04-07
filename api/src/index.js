2abce810d3f5d4da50d5e2f4705faf90251c7ef0
aleksanderwawrzyniak <aleksanderwawrzyniak98@gmail.com>
2020-04-07 19:01:47 +0200

    add / route and handle db connection error

--- api/src/index.js
+++ api/src/index.js
@@ -4,28 +4,41 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 // Import routes
 const authRoute = require('./routes/auth');
-const postRoute = require('./routes/tests');
+const postRoute = require('./routes/test');
 
 // you will have to create your own .env file in the root of the backend
 // it is not recommended to commit .env files;
 dotenv.config();
 
 // Connect to DB
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
-  console.log('connected to db!')
-);
+try {
+    mongoose.connect(
+        process.env.DB_CONNECT,
+        { useNewUrlParser: true },
+        (error) => {
+            if (error) throw error;
+            console.log('connected to db!');
+        }
+    );
 
-const app = express();
+    const app = express();
 
-// Middleware
-app.use(bodyParser.json());
+    // Middleware
+    app.use(bodyParser.json());
 
-// Route Middleware
-app.use('/api/user', authRoute);
-app.use('/api/tests', postRoute);
+    // Route Middleware
+    app.use('/api/user', authRoute);
+    app.use('/api/test', postRoute);
 
-const port = process.env.SERVER_PORT || 3000;
+    app.get('/', (req, res) => {
+        res.send('Hello there fellow developer');
+    });
 
-app.listen(port, () => {
-  console.log(`server is now listening for requests on port ${port}`);
-});
+    const port = process.env.SERVER_PORT || 3000;
+
+    app.listen(port, () => {
+        console.log(`server is now listening for requests on port ${port}`);
+    });
+} catch (e) {
+    console.log(e);
+}
