6919495e079e21aa0d08160648eceee7c1e717c6
aleksanderwawrzyniak <aleksanderwawrzyniak98@gmail.com>
2020-04-07 19:19:14 +0200

    add get /, db connection handle and models

--- api/src/validate.js
+++ api/src/validate.js
@@ -1,26 +1,31 @@
 const Joi = require('@hapi/joi');
 
 const registerValidation = (data) => {
-  const schema = Joi.object({
-    username: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required(),
-  });
+    const schema = Joi.object({
+        username: Joi.string().min(2).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required(),
+        firstName: Joi.string().min(2).max(100),
+        lastName: Joi.string().min(2).max(100),
+    });
 
-  return schema.validate(data, {
-    abortEarly: false,
-  });
+    return schema.validate(data, {
+        abortEarly: false,
+    });
 };
 
 const loginValidation = (data) => {
-  const schema = Joi.object({
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required(),
-  });
+    const schema = Joi.object({
+        // TODO: decide how the user will log in, whether we will use username / login
+        // or email.
+        // username: Joi.string().min(2).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required(),
+    });
 
-  return schema.validate(data, {
-    abortEarly: false,
-  });
+    return schema.validate(data, {
+        abortEarly: false,
+    });
 };
 
 module.exports.registerValidation = registerValidation;
