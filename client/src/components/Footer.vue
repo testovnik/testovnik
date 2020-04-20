9fad457e50e27b02bc9c98d2cd58e26b1172c5f0
Filip <fil.wojtowicz@gmail.com>
2020-04-20 17:12:24 +0200

    add header, other stuff WIP

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="footer">
+    artwork provided by
+    <a class="footer" href="https://absurd.design/">absurd design</a>
+  </div>
+</template>
+
+<script>
+export default { name: "Footer" };
+</script>
+
+<style lang="sass" scoped>
+.footer
+    color: $dark-gray
+</style>
---
+++ client/src/components/LandingPageUnregistered.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="container">
+      <img
+        class="doodle"
+        src="@/assets/component.png"
+        alt="Artwork by https://absurd.design/illustrations.html"
+      />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+export default {
+  name: "LandingPageUnregistered",
+  components: {
+    Navbar,
+    Footer
+  }
+};
+</script>
+
+<style></style>
