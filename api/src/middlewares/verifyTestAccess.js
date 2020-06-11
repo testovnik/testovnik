f40105e8243ed0a56c193d14ed7c3d584638a7b4
Aleksander Wawrzyniak <aleksander.wawrzyniak98@gmail.com>
2020-06-11 12:21:11 +0200

    fix test modification authorization error

--- api/src/middlewares/verifyTestAccess.js
+++ api/src/middlewares/verifyTestAccess.js
@@ -4,8 +4,15 @@ module.exports = async function (req, res, next) {
     const { test_id } = req.params;
     try {
         const test = await Test.findById(test_id);
-        if (!test.authors.includes(req.user._id))
+		const authorized_authors = test.authors;
+		const { id } = req.user;
+
+		const isAuthorizedUser = authorized_authors.reduce((accumulator, currentValue) => {
+			return accumulator || currentValue.id === id;
+		}, false);
+		if (!isAuthorizedUser)
             return res.status(400).json({ error: 'Unauthorized attempt' });
+
         next();
     } catch (err) {
         res.status(400).send(err);
