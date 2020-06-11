d9c563de885419766ff39328d528d818261ca001
Aleksander Wawrzyniak <aleksander.wawrzyniak98@gmail.com>
2020-06-11 12:38:04 +0200

    fix formatting

--- api/src/middlewares/verifyTestAccess.js
+++ api/src/middlewares/verifyTestAccess.js
@@ -1,16 +1,19 @@
 const Test = require('../models/Test');
 
-module.exports = async function (req, res, next) {
+module.exports = async function(req, res, next) {
     const { test_id } = req.params;
     try {
         const test = await Test.findById(test_id);
-		const authorized_authors = test.authors;
-		const { id } = req.user;
+        const authorized_authors = test.authors;
+        const { id } = req.user;
 
-		const isAuthorizedUser = authorized_authors.reduce((accumulator, currentValue) => {
-			return accumulator || currentValue.id === id;
-		}, false);
-		if (!isAuthorizedUser)
+        const isAuthorizedUser = authorized_authors.reduce(
+            (accumulator, currentValue) => {
+                return accumulator || currentValue.id === id;
+            },
+            false
+        );
+        if (!isAuthorizedUser)
             return res.status(400).json({ error: 'Unauthorized attempt' });
 
         next();
