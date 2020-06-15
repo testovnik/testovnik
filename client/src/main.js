7ae187a7ce14837b5a5d37e0b0cbb51a41867315
Adriaen6 <adr.andry@hotmail.com>
2020-06-15 12:35:34 +0200

    local storage token fix
    Co-authored-by: Aleksander Wawrzyniak <wotf0rt1s@gmail.com>

--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from '@/axios'
 
 Vue.config.productionTip = false
-
+if (localStorage.getItem('user-token')) {
+  axios.defaults.headers.common['x-auth-token'] = localStorage.getItem('user-token')
+}
 new Vue({
   router,
   store,
